29ef1a0575122196cb692eb5d7603e5a38e386f8
Bjornarnepaulsen <bjornarnepaulsen@hotmail.com>
2025-07-02 10:51:29 +0200

    Updated gameloop to use pygame groups

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ import pygame
 from constants import *
 from player import Player
 
+
+#Main loop
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
+    #Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-
+        
     while True:
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
@@ -21,8 +28,9 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         dt = clock.tick(60)  # Limit to 60 FPS
         pygame.display.flip()
     
